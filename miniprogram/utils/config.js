d02bcff7c3254d9b5bd2a61beb6882dc683ec9ad
Anna <1348596564@qq.com>
2020-11-16 14:18:09 +0800

    提交云菜谱

---
+++ miniprogram/utils/config.js
@@ -0,0 +1,14 @@
+const tables={
+  user:'vx_user',
+  cate:'vx_cate',
+  menu:"vx_menu",
+  user_cate:"vx_user_cate",
+  collect:"vx_collect"
+  
+}
+// let admin="oFttM5Lkew3TVjPkRkIq9Nc5gbuA"; 同桌的
+let admin="oFttM5Lkew3TVjPkRkIq9Nc5gbuA"
+export {
+  tables,
+  admin
+}
